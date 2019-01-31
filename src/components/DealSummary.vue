66478d217e87ed93239d31f821d999e5a816590a
simonvasseur <simonvasseur91550@gmail.com>
2019-01-31 09:54:42 +0100

    dealpage css renaming

--- src/components/DealSummary.vue
+++ src/components/DealSummary.vue
@@ -43,7 +43,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
     .deal-summary {
         background-color: white;
         height: 265px;
--- src/components/pages/DealPage.vue
+++ src/components/pages/DealPage.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="deal-page">
-        <div class="deal-summary">
-            <div class="deal-summary-container">
-                <div class="deal-summary-image">
+        <div class="deal-header">
+            <div class="deal-header-container">
+                <div class="deal-header-image">
                     <img src="../../assets/switch.jpg">
                 </div>
-                <div class="deal-summary-informations">
-                    <div class="deal-summary-title">
+                <div class="deal-header-informations">
+                    <div class="deal-header-title">
                         <span>Red Dead Redemption 2 : Bonus Précommande</span>
                     </div>
-                    <div class="deal-summary-price">
+                    <div class="deal-header-price">
                         <span class="price">65,99€</span> <span class="shipping">+5,99€ | sur micromania.fr</span> 
                     </div>
-                    <div class="deal-summary-poster">
+                    <div class="deal-header-poster">
                         <img src="../../assets/julien.jpg" alt=""> <span><b>Julien</b> - Publié il y a 3 jours - Expire le 05/02/2019</span>
                     </div>
                     <div class="col-md-12">
-                        <div class="deal-summary-votes col-md-6">
+                        <div class="deal-header-votes col-md-6">
                             <div class="vote-button">
                                 <span class="plus">+</span>
                             </div>
@@ -39,8 +39,16 @@
                     </div>
                 </div>
             </div>
+            <div class="col-md-12 description-container">
+                <span>Amérique, 1899. L'ère de l'Ouest sauvage touche à sa fin alors que les autorités ont décidé de traquer les dernières bandes de hors-la- loi qui sévissent encore. Ceux qui ne se rendent pas ou résistent sont tués. <br><br>
+
+Suite à un braquage qui a mal tourné dans la ville de Blackwater, Arthur Morgan et le reste des hors-la-loi de la bande de Dutch van der Linde doivent prendre la fuite vers l'est. Les agents fédéraux et les meilleurs chasseurs de primes du pays se mettent à leurs trousses et la bande commet méfaits sur
+méfaits dans les vastes terres sauvages de l'Amérique dans un seul et unique but : survivre. <br><br> Alors que des querelles internes menacent de faire voler la bande en éclats, Arthur est tiraillé entre ses propres idéaux et sa loyauté envers la bande qui l'a élevé.
+
+Par les créateurs de Grand Theft Auto V et Red Dead Redemption, Red Dead Redemption 2 raconte une histoire épique au cœur de l'Amérique à l'aube de l'ère moderne.</span>
+            </div>
         </div>
-        </div>
+    </div>
 </template>
 
 <script>
@@ -49,72 +57,74 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
     .deal-page {
         width:60%;
-        margin: auto
-    }
-    .deal-summary {
+        margin: auto;
         background-color: white;
+        display: inline-block;
+        min-height: calc(100vh - 80px);
+        margin-top:20px
+    }
+    .deal-header {
         height: 265px;
-        margin-top:20px;
     }
 
-    .deal-summary-container {
+    .deal-header-container {
         width: 100%;
     }
 
-    .deal-summary-container .col-md-12 {
+    .deal-header-container .col-md-12 {
         padding:0;
         border-bottom: 0.1px solid gainsboro;
     }
 
-    .deal-summary-container hr {
+    .deal-header-container hr {
         margin-right:20px
     }
 
-    .deal-summary-image {
+    .deal-header-image {
         float: left;
         padding: 20px;
         
     }
 
-    .deal-summary-image > img {
+    .deal-header-image > img {
         width:225px;
         height:225px;
     }
 
-    .deal-summary-informations {
+    .deal-header-informations {
         padding-top:15px;
         float: left;
         text-align: left;
         width: calc(100% - 280px);
     }
 
-    .deal-summary-title {
+    .deal-header-title {
         width: 100%;
         float: left;
     }
 
-    .deal-summary-title > span {
+    .deal-header-title > span {
         font-size: 22px
     }
 
-    .deal-summary-price {
+    .deal-header-price {
         width: 100%;
         float: left;
     }
 
-    .deal-summary-price > .price {
+    .deal-header-price > .price {
         font-size: 22px
     }
 
-    .deal-summary-price > .shipping {
+    .deal-header-price > .shipping {
         font-size: 18px;
         color:#B8B1B1;
     }
 
-    .deal-summary-description {
+    .deal-header-description {
         width: 100%;
         overflow: hidden;
     }
@@ -134,11 +144,11 @@ export default {
         background: linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 100%);
     }
 
-    .deal-summary-description > span {
+    .deal-header-description > span {
         font-size: 18px
     }
 
-    .deal-summary-poster {
+    .deal-header-poster {
         width:100%;
         margin-top:8px;
         float: left;
@@ -146,24 +156,24 @@ export default {
         border-bottom: 0.1px solid gainsboro;
     }
 
-    .deal-summary-poster > img {
+    .deal-header-poster > img {
         width:30px;
     }
 
-    .deal-summary-poster > span {
+    .deal-header-poster > span {
         font-size: 15px;
         position: absolute;
         padding-left: 10px;
         padding-top: 3px;
     }
 
-    .deal-summary-votes {
+    .deal-header-votes {
         padding: 0;
         margin-top: 10px;
         float: left;
     }
 
-    .deal-summary-votes span {
+    .deal-header-votes span {
         font-size: 25px;
         float: left;
         margin-top: -3px;
@@ -236,5 +246,15 @@ export default {
         margin-right:20px;
         cursor: pointer;
         margin-top:15px;
+    }
+
+    .description-container {
+        text-align: left;
+        float: left;
+        padding-left:20px;
+    }
+
+    .description-container > span {
+        font-size: 15px;
     }
 </style>
